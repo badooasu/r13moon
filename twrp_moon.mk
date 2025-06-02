b4f1944145a056ab4d3044ec7f3e690885922432
badooasu <badooasu@gmail.com>
2025-06-02 14:17:11 +0700

    Update and rename omni_boot.mk to twrp_moon.mk

--- twrp_moon.mk
+++ twrp_moon.mk
@@ -8,18 +8,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from boot device
-$(call inherit-product, device/generic/boot/device.mk)
+# Inherit from moon device
+$(call inherit-product, device/xiaomi/moon.mk)
 
-PRODUCT_DEVICE := boot
-PRODUCT_NAME := omni_boot
-PRODUCT_BRAND := generic
-PRODUCT_MODEL := Generic Device
-PRODUCT_MANUFACTURER := generic
+PRODUCT_DEVICE := moon
+PRODUCT_NAME := twrp_moon
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := moon
+PRODUCT_MANUFACTURER := xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-generic
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC=""
